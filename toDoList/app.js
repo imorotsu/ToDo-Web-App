ec8e808bf0a460214b5c1569785c29e6a23401c2
imorotsu <49424215+imorotsu@users.noreply.github.com>
2020-01-19 22:59:20 +1100

    Add files via upload

---
+++ toDoList/app.js
@@ -0,0 +1,45 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const date = require(__dirname + "/date.js");
+const items = [
+	"Buy Food",
+	"Cook Food",
+	"Eat Food"
+];
+const workItems = [];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// root route
+app.get("/", function(req, res){
+	let day = date.getDay();
+	res.render("list", { listTitle: day, newListItems: items });
+});
+app.post("/", function(req, res){
+	let item = req.body.newItem;
+	if (req.body.list === "Work") {
+		workItems.push(item);
+		res.redirect("/work");
+	} else {
+		items.push(item);
+		res.redirect("/");
+	}
+});
+app.get("/work", function(req, res){
+	res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+app.post("/work", function(req, res){
+	let item = req.body.newItem;
+	workItems.push(item);
+	res.redirect("/work");
+});
+//about page
+app.get("/about", function(req, res){
+	res.render("about");
+});
+// start server on port 3000
+app.listen(3000, function(){
+	console.log("Server has started on PORT 3000");
+});
